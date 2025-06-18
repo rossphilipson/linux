ce4a3249a0a2332b619ca40d6cda2bc613d740f8
Ross Philipson <ross.philipson@oracle.com>
2025-06-18 09:50:25 -0700

    tpm: Move TPM common base definitions to new public common header
    These are top level definitions shared by both TPM 1 and 2
    family chips.
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>

--- include/linux/tpm.h
+++ include/linux/tpm.h
@@ -29,40 +29,11 @@
 #include "tpm1.h"
 #include "tpm2.h"
 
-#define TPM_DIGEST_SIZE 20	/* Max TPM v1.2 PCR size */
-#define TPM_MAX_DIGEST_SIZE SHA512_DIGEST_SIZE
-
 struct tpm_chip;
 struct trusted_key_payload;
 struct trusted_key_options;
 /* opaque structure, holds auth session parameters like the session key */
 struct tpm2_auth;
-
-/* if you add a new hash to this, increment TPM_MAX_HASHES below */
-enum tpm_algorithms {
-	TPM_ALG_ERROR		= 0x0000,
-	TPM_ALG_SHA1		= 0x0004,
-	TPM_ALG_AES		= 0x0006,
-	TPM_ALG_KEYEDHASH	= 0x0008,
-	TPM_ALG_SHA256		= 0x000B,
-	TPM_ALG_SHA384		= 0x000C,
-	TPM_ALG_SHA512		= 0x000D,
-	TPM_ALG_NULL		= 0x0010,
-	TPM_ALG_SM3_256		= 0x0012,
-	TPM_ALG_ECC		= 0x0023,
-	TPM_ALG_CFB		= 0x0043,
-};
-
-/*
- * maximum number of hashing algorithms a TPM can have.  This is
- * basically a count of every hash in tpm_algorithms above
- */
-#define TPM_MAX_HASHES	5
-
-struct tpm_digest {
-	u16 alg_id;
-	u8 digest[TPM_MAX_DIGEST_SIZE];
-} __packed;
 
 struct tpm_bank_info {
 	u16 alg_id;
@@ -140,12 +111,6 @@ struct tpm_chip_seqops {
  */
 #define TPM2_MAX_CONTEXT_SIZE 4096
 
-/*
- * The maximum locality (0 - 4) for a TPM, as defined in section 3.2 of the
- * Client Platform Profile Specification.
- */
-#define TPM_MAX_LOCALITY		4
-
 struct tpm_chip {
 	struct device dev;
 	struct device devs;
@@ -216,8 +181,6 @@ struct tpm_chip {
 #endif
 };
 
-#define TPM_HEADER_SIZE		10
-
 static inline enum tpm2_mso_type tpm2_handle_mso(u32 handle)
 {
 	return handle >> 24;
@@ -245,15 +208,6 @@ enum tpm_chip_flags {
 };
 
 #define to_tpm_chip(d) container_of(d, struct tpm_chip, dev)
-
-struct tpm_header {
-	__be16 tag;
-	__be32 length;
-	union {
-		__be32 ordinal;
-		__be32 return_code;
-	};
-} __packed;
 
 enum tpm_buf_flags {
 	/* the capacity exceeded: */
