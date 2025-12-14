534dbfbdd80d833b46de91ed4cda12ab671a8300
Jarkko Sakkinen <jarkko@kernel.org>
2025-12-14 17:38:03 +0200

    tpm: Orchestrate TPM commands in tpm_get_random()
    tpm1_get_random() and tpm2_get_random() contain duplicate orchestration
    code. Consolidate orchestration to tpm_get_random().
    
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>
    Message-ID: <20251214153808.73831-7-jarkko@kernel.org>

--- drivers/char/tpm/tpm.h
+++ drivers/char/tpm/tpm.h
@@ -252,7 +252,6 @@ int tpm1_pcr_extend(struct tpm_chip *chip, u32 pcr_idx, const u8 *hash,
 int tpm1_pcr_read(struct tpm_chip *chip, u32 pcr_idx, u8 *res_buf);
 ssize_t tpm1_getcap(struct tpm_chip *chip, u32 subcap_id, cap_t *cap,
 		    const char *desc, size_t min_cap_length);
-int tpm1_get_random(struct tpm_chip *chip, u8 *out, size_t max);
 int tpm1_get_pcr_allocation(struct tpm_chip *chip);
 unsigned long tpm_calc_ordinal_duration(struct tpm_chip *chip, u32 ordinal);
 int tpm_pm_suspend(struct device *dev);
@@ -292,7 +291,6 @@ int tpm2_pcr_read(struct tpm_chip *chip, u32 pcr_idx,
 		  struct tpm_digest *digest, u16 *digest_size_ptr);
 int tpm2_pcr_extend(struct tpm_chip *chip, u32 pcr_idx,
 		    struct tpm_digest *digests);
-int tpm2_get_random(struct tpm_chip *chip, u8 *dest, size_t max);
 ssize_t tpm2_get_tpm_pt(struct tpm_chip *chip, u32 property_id,
 			u32 *value, const char *desc);
 
